94d7b1db255c9e5a52f9973b5e7acf436e34f236
Scott Keith Thomas, Jr <skthomasjr@users.noreply.github.com>
2016-06-14 19:07:52 -0600

    Add files via upload

---
+++ Source/CrossChannel.SyncObjects/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CrossChannel.SyncObjects")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CrossChannel.SyncObjects")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("38870b90-ceaa-4662-860c-119b429b1753")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
